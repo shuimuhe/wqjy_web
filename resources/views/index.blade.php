376e07c5e7fb4ea6991e272c7078b17b634c20ac
Adino <shuimuhe_86@163.com>
2017-06-02 09:46:32 +0800

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>万桥酒业</title>
+<style>
+    A:link {
+        color: #000000;
+        TEXT-DECORATION: none
+    }
+    A:visited {
+        COLOR: #000000;
+        TEXT-DECORATION: none
+    }
+    A:hover {
+        COLOR: #ff7f24;
+        text-decoration: underline;
+    }
+    A:active {
+        COLOR: #ff7f24;
+        text-decoration: underline;
+    }
+    #header {
+        width: 100%;
+    }
+    #nav{
+        margin: 0 auto;
+        width: 600px;
+        padding-top: 20px;
+    }
+    .header_left {
+        padding-left: 60px;
+    }
+    #bannar {
+        width: 100%;
+    }
+</style>
+</head>
+
+<body>
+<div id="header">
+    <div id="nav">
+        <a href="#" class="juedui">首页</a>
+        <a href="#" class="juedui header_left">精彩活动</a>
+        <a href="#" class="juedui header_left">品牌产品</a>
+        <a href="#" class="juedui header_left">联系我们</a>
+        <a href="#" class="juedui header_left">关于万桥</a>
+    </div>
+</div>
+<div id="bannar">
+
+</div>
+</body>
+</html>
